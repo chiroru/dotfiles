fd6b9a468829067c4ea217aa1613fdd00c1524a2
chiroru <chiroru_0130@yahoo.co.jp>
2013-04-30 14:43:53 +0900

    セットアップスクリプトを変更.

--- setup.sh
+++ setup.sh
@@ -5,36 +5,30 @@
 #
 # ======================================================================
 
-if [ ! -d ${HOME}/dotfiles/vimfiles/vundle.git ]
-then
+if [ ! -f ${HOME}/.bashrc ]; then
+    /usr/bin/ln -s ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
+fi
+;J
+if [ ! -f ${HOME}/.screenrc ]; then
+    /usr/bin/ln -s ${HOME}/dotfiles/.screenrc ${HOME}/.screenrc
+fi
+
+if [ ! -f ${HOME}/.vimrc ]; then
+    /usr/bin/ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
+fi
+
+if [ ! -d ${HOME}/.vim ]; then
+    /usr/bin/ln -s ${HOME}/dotfiles/vimfiles ${HOME}/.vim
+fi
+
+if [ ! -d ${HOME}/dotfiles/vimfiles/vundle.git ]; then
     CUR_DIR=`pwd`
     cd ${HOME}/dotfiles
     git submodule add http://github.com/gmarik/vundle.git ${HOME}/dotfiles/vimfiles/vundle.git
     cd ${CUR_DIR}
 fi
 
-if [ ! -f ${HOME}/.screenrc ]
-then
-    /usr/bin/ln -s ${HOME}/dotfiles/.screenrc ${HOME}/.screenrc
-fi
-
-if [ ! -f ${HOME}/.vimrc ]
-then
-    /usr/bin/ln -s ${HOME}/dotfiles/.vimrc ${HOME}/.vimrc
-fi
-
-if [ ! -f ${HOME}/.bashrc ]
-then
-    /usr/bin/ln -s ${HOME}/dotfiles/.bashrc ${HOME}/.bashrc
-fi
-
-if [ ! -d ${HOME}/.vim ]
-then
-    /usr/bin/ln -s ${HOME}/dotfiles/vimfiles ${HOME}/.vim
-fi
-
-if [ ! -d ${HOME}/dotfiles/vimfiles/bundle ]
-then
+if [ ! -d ${HOME}/dotfiles/vimfiles/bundle ]; then
     /usr/bin/mkdir ${HOME}/dotfiles/vimfiles/bundle
     /usr/bin/ln -s ${HOME}/dotfiles/vimfiles/vundle.git ${HOME}/dotfiles/vimfiles/bundle/vundle
 fi
